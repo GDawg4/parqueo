225e047e7a18917456c73818b06e5d186c49b0cd
GDawg4 <garoz.r4@gmail.com>
2018-08-05 10:18:59 -0600

    Funcionalidades iniciales

--- src/main/main.kt
+++ src/main/main.kt
@@ -1,17 +1,56 @@
 package main
 
-fun showMenu(optionNumber: String){
-    if (optionNumber == "1"){
+import java.io.File
+import java.io.InputStream
 
+fun showMenu(optionNumber: String): String{
+    when (optionNumber){
+        "0"->
+        return("""
+            Menu:
+            Elija el tipo de usuario que desea utilizar
+            1. Administrador
+            2. Conductor
+            3. Salir
+        """.trimIndent())
+    "1" ->
+        return("""
+            Menu:
+            1. Crear nivel
+            2. Eliminar nivel
+            3. Ver todos los niveles
+            4. Salir
+        """.trimIndent())
+    "2"->
+        return("""
+            Menu:
+            1. Ingresar placa
+            2. Salir
+        """.trimIndent())
     }
+    return ""
+}
+
+fun readLines(fileName: String){
+    var mapAsLines: ArrayList<String> = ArrayList()
+    val file: InputStream = File(fileName).inputStream()
+    file.bufferedReader().useLines { lines->lines.forEach { mapAsLines.add(it)} }
 }
 
 fun main(args: Array<String>) {
     var levelList: ArrayList<Nivel>
-    var wantsToContinue: Boolean = true
+    var wantsToContinueInProgram: Boolean = true
+    var wantsToContinueAsUser: Boolean = true
+    var wantsToContinueAsAdin: Boolean = true
+    var adminOrUser: String
+
+    //main.readLines("C:\\Users\\garoz\\Desktop\\2018 2\\parqueo\\prueba.txt")
 
     do {
+        println(showMenu("1"))
+        adminOrUser = readLine()!!
+        if (adminOrUser == "1"){
+        }
 
-
-    }while (wantsToContinue)
+    }while (wantsToContinueInProgram)
 }
